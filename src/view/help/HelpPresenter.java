4ce880f18e5b6940930ccb99d5ba4dfca3c6bbcd
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2022-05-10 22:01:23 +0200

    rules worden ingelezen op HelpView

---
+++ src/view/help/HelpPresenter.java
@@ -0,0 +1,25 @@
+package view.help;
+
+import model.Game;
+
+public class HelpPresenter {
+    private Game model;
+    private HelpView view;
+    public HelpPresenter(Game model,
+                             HelpView view) {
+        this.model = model;
+        this.view = view;
+        this.addEventHandlers();
+        this.updateView();
+    }
+
+
+    private void addEventHandlers() {
+
+    }
+
+    private void updateView() {
+
+    }
+
+}
