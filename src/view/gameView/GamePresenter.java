096f8a1107f5693d67e6cde7b116aeecdbec574d
Dave.smedts <73465667+davesmedts@users.noreply.github.com>
2022-05-16 21:06:51 +0200

    frontend movepiece

--- src/view/gameView/GamePresenter.java
+++ src/view/gameView/GamePresenter.java
@@ -86,6 +86,7 @@ public class GamePresenter {
                                         frontendSquare.setStyle("-fx-background-color:GREEN");
                                     }
                                 }
+
                             }
                             selectCounter++;
                         } else {
