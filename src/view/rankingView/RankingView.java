7e3dd7b8de40aa38bdf64eb7759d1ee5d176feeb
Mattia Verreydt <mattiaver@hotmail.com>
2022-05-21 22:08:38 +0200

    lay-out table ok, maar nog niet resizable

--- src/view/rankingView/RankingView.java
+++ src/view/rankingView/RankingView.java
@@ -1,13 +1,14 @@
 package view.rankingView;
 
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 
-public class RankingView extends BorderPane {
+public class RankingView extends GridPane {
     private HBox mainContainer;
     private HBox helpIconsContainer;
     private VBox leftContainer;
@@ -90,24 +91,68 @@ public class RankingView extends BorderPane {
         final Menu bestandMenu = new Menu("Bestand",null, this.openen, this.opslaan, this.afsluiten);
         final Menu helpMenu = new Menu("Help",null, this.spelregels, this.info);
         final MenuBar menuBar = new MenuBar(bestandMenu, helpMenu);
-        this.setTop(menuBar);
+        this.add(menuBar, 0 ,0,3,1);
 
 //        MainContainerContent
         table.getColumns().addAll(datumCol, spelerWitCol, spelerZwartCol,winnaarCol);
         mainContainer.getChildren().addAll(table);
-        this.setCenter(mainContainer);
+        this.add(mainContainer, 1 ,2);
+        datumCol.setPrefWidth(150);
+        spelerWitCol.setPrefWidth(150);
+        spelerZwartCol.setPrefWidth(150);
+        winnaarCol.setPrefWidth(150);
+
+
+
 
 
 //        leftArea
         leftContainer.getChildren().addAll(chessLogo, homeBtn);
-        this.setLeft(leftContainer);
+        this.add(leftContainer, 0 ,1);
 
 //      HelpIcons
         helpIconsContainer.getChildren().addAll(settingsIcon, helpIcon, infoIcon);
-        this.setRight(helpIconsContainer);
+        this.add(helpIconsContainer, 2 ,1);
 
         //        statusbar
-        this.setBottom(statusBarText);
+        this.add(statusBarText,1,3,1,1);
+
+        this.setGridLinesVisible(true);
+
+
+
+        // column constraints
+        ColumnConstraints col1Constraints = new ColumnConstraints();
+        col1Constraints.setPercentWidth(30);
+        ColumnConstraints col2Constraints = new ColumnConstraints();
+        col2Constraints.setPercentWidth(40);
+        ColumnConstraints col3Constraints = new ColumnConstraints();
+        col3Constraints.setPercentWidth(30);
+
+        this.getColumnConstraints().addAll(col1Constraints, col2Constraints, col3Constraints);
+
+
+        // constraints Gridpane
+        this.setConstraints(menuBar,     0, 0, 3, 1,
+                HPos.CENTER,       VPos.TOP,
+                Priority.ALWAYS, Priority.ALWAYS);
+
+
+        this.setConstraints(mainContainer,     1, 2, 1, 1,
+                HPos.CENTER,       VPos.CENTER,
+                Priority.ALWAYS, Priority.ALWAYS);
+
+        this.setConstraints(helpIconsContainer,     2, 1, 1, 1,
+                HPos.LEFT,       VPos.CENTER,
+                Priority.ALWAYS, Priority.ALWAYS);
+
+
+        this.setConstraints(statusBarText,     1, 3, 1, 1,
+                HPos.CENTER,       VPos.BOTTOM,
+                Priority.ALWAYS, Priority.ALWAYS);
+
+
+
     }
 
     public Button getHomeBtn() {
